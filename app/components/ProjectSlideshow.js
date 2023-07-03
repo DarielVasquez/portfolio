b76773202c1345d3ecfd923c152d285ebdd4b3e1
Dariel Vasquez <darielalejandro10@gmail.com>
2023-07-03 14:18:14 -0600

    fixed error in project slideshow for deployment

--- app/components/ProjectSlideshow.js
+++ app/components/ProjectSlideshow.js
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import ProjectModal from "./ProjectModal";
 
 const ImageSlider = ({ images, urlProject }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
