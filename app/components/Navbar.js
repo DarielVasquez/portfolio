8293a85eb8c6632d89a2a73e3a9899a2f489e0d3
Dariel Vasquez <darielalejandro10@gmail.com>
2023-07-03 14:51:54 -0600

    visual changes to navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -49,32 +49,37 @@ const Navbar = () => {
       className="mix-blend-exclusion text-sm md:text-xl font-light fixed w-full z-50 transition-all ease-in-out duration-300 bg-white bg-opacity-20 lg:bg-transparent lg:bg-opacity-0"
     >
       <div className="container mx-auto py-4 px-8">
-        <ul className="text-stone-400 flex justify-evenly lg:justify-end gap-3 sm:gap-5">
+        <ul className="text-stone-300 flex justify-evenly lg:justify-end gap-3 sm:gap-5">
           <li
+            aria-haspopup="true"
             className="lg:mr-auto cursor-pointer"
             onClick={() => scrollToTop()}
           >
             Home
           </li>
           <li
+            aria-haspopup="true"
             className="cursor-pointer"
             onClick={() => handleScrollToElement("about")}
           >
             About
           </li>
           <li
+            aria-haspopup="true"
             className="cursor-pointer"
             onClick={() => handleScrollToElement("skills")}
           >
             Skills
           </li>
           <li
+            aria-haspopup="true"
             className="cursor-pointer"
             onClick={() => handleScrollToElement("projects")}
           >
             Projects
           </li>
           <li
+            aria-haspopup="true"
             className="cursor-pointer"
             onClick={() => handleScrollToElement("contact")}
           >
