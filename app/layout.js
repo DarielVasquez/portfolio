a7156294ebc73565985f4f02d1bedc767feae2de
Dariel Vasquez <darielalejandro10@gmail.com>
2023-07-04 22:45:10 -0600

    added tailwind plugin for scrollbar

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html className="scroll-smooth" lang="en">
+    <html
+      className="scroll-smooth scrollbar-thin scrollbar-thumb-rounded-md scrollbar-thumb-teal-900 scrollbar-track-stone-800"
+      lang="en"
+    >
       <body className={inter.className}>
         <Navbar></Navbar>
         {children}
